4951363269e898922604abfb2a8dfba88c710246
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-02-24 17:59:06 +0900

    Solved Electronics Shop

---
+++ python3/hackerrank/Solution_ElectronicsShop.py
@@ -0,0 +1,14 @@
+def getMoneySpent(keyboards, drives, b):
+    res = -1
+    for k in keyboards:
+        for d in drives:
+            sum = k + d
+            if sum <= b and sum > res:
+                res = sum
+    return res
+
+res = getMoneySpent([3, 1], [5, 2, 8], 10)
+assert 9 == res
+res = getMoneySpent([4], [5], 5)
+assert -1 == res
+print('All good!')
