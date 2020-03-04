501a320ab16d2946ac677d8bf6dbc286708cff84
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-04 19:15:14 +0900

    Solved 2 tasks

---
+++ python3/hackerrank/Solution_PickingNumbers.py
@@ -0,0 +1,20 @@
+def pickingNumbers(a):
+    a.sort()
+    max_num = 0
+    big_index = 0
+    small_index = 0
+    counter = 0
+    while big_index < len(a):
+        if small_index < len(a) and a[small_index] - a[big_index] <= 1:
+            counter += 1
+            small_index += 1
+            continue
+        
+        max_num = max_num if max_num > counter else counter
+        counter = 0
+        big_index += 1
+        small_index = big_index
+    return max_num
+
+assert 5 == pickingNumbers([ 1, 2, 2, 3, 1, 2 ])
+assert 3 == pickingNumbers([ 4, 6, 5, 3, 3, 1 ])
---
+++ python3/hackerrank/Solution_TheHurdleRace.py
@@ -0,0 +1,3 @@
+def hurdleRace(k, height):
+    a = max(height) - k
+    return a if a > 0 else 0
