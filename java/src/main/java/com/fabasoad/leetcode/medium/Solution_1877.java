160e9e8818376293aa44bf5bf3aaa67e0c60cb10
fabasoad <fabasoad@gmail.com>
2022-01-23 10:16:03 +0900

    Temp solution for leetcode: 1877 (2)

--- java/src/main/java/com/fabasoad/leetcode/medium/Solution_1877.java
+++ java/src/main/java/com/fabasoad/leetcode/medium/Solution_1877.java
@@ -1,30 +1,27 @@
 package com.fabasoad.leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution_1877 {
 
-  static int prev = 0;
+  int prev = 0;
 
-  private static void pair(int[] nums, List<int[]> list, List<List<int[]>> total) {
-    for (int i = 1; i < nums.length; i++) {
+  private void pair(List<Integer> nums, List<int[]> list, List<List<int[]>> total) {
+    for (int i = 1; i < nums.size(); i++) {
       if (prev != total.size()) {
-        for (int j = 0; j < nums.length / 2; j++) {
+        for (int j = 0; j < nums.size() / 2; j++) {
           list.remove(list.size() - 1);
         }
         prev = total.size();
       }
-      list.add(new int[] { nums[0], nums[i] });
-      int[] arr = new int[nums.length - 2];
-      int x = 0;
-      for (int j = 1; j < nums.length; j++) {
-        if (j != i) {
-          arr[x] = nums[j];
-          x++;
-        }
-      }
-      if (arr.length == 0) {
+      list.add(new int[] { nums.get(0), nums.get(i), nums.get(0) + nums.get(i) });
+      List<Integer> arr = new ArrayList<>(nums);
+      arr.remove(0);
+      arr.remove(i - 1);
+      if (arr.isEmpty()) {
         total.add(new ArrayList<>(list));
       } else {
         pair(arr, list, total);
@@ -32,15 +29,33 @@ public class Solution_1877 {
     }
   }
 
-  public static int minPairSum(int[] nums) {
+  public int minPairSum(int[] nums) {
     final List<List<int[]>> total = new ArrayList<>();
-    pair(nums, new ArrayList<>(), total);
-    return 0;
+    pair(Arrays.stream(nums).boxed().collect(Collectors.toList()), new ArrayList<>(), total);
+    final int[] f = new int[total.size()];
+    for (int i = 0; i < total.size(); i++) {
+      final List<int[]> a = total.get(i);
+      int max = -1;
+      for (int[] b : a) {
+        final int sum = b[2];
+        if (max < sum) {
+          max = sum;
+          f[i] = max;
+        }
+      }
+    }
+    int min = Integer.MAX_VALUE;
+    for (int j : f) {
+      if (min > j) {
+        min = j;
+      }
+    }
+    return min;
   }
 
   public static void main(String[] args) {
-//    assert 7 == minPairSum(new int[] { 3,5,3,2 });
-//    assert 8 == minPairSum(new int[] { 3,5,4,2,4,6 });
-    assert 6 == minPairSum(new int[] { 3,2,4,1,1,5,1,3,5,1 });
+    assert 7 == new Solution_1877().minPairSum(new int[] { 3,5,3,2 });
+    assert 8 == new Solution_1877().minPairSum(new int[] { 3,5,4,2,4,6 });
+    assert 6 == new Solution_1877().minPairSum(new int[] { 3,2,4,1,1,5,1,3,5,1 });
   }
 }
