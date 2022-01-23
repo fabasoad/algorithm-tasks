844c7b304d161858ef2f5332227f968658e501b0
fabasoad <fabasoad@gmail.com>
2022-01-23 10:19:59 +0900

    Temp solution for leetcode: 1877 (3)

--- java/src/main/java/com/fabasoad/leetcode/medium/Solution_1877.java
+++ java/src/main/java/com/fabasoad/leetcode/medium/Solution_1877.java
@@ -9,38 +9,37 @@ public class Solution_1877 {
 
   int prev = 0;
 
-  private void pair(List<Integer> nums, List<int[]> list, List<List<int[]>> total) {
+  private void pair(List<Integer> nums, List<Integer> maximums, List<List<Integer>> total) {
     for (int i = 1; i < nums.size(); i++) {
       if (prev != total.size()) {
         for (int j = 0; j < nums.size() / 2; j++) {
-          list.remove(list.size() - 1);
+          maximums.remove(maximums.size() - 1);
         }
         prev = total.size();
       }
-      list.add(new int[] { nums.get(0), nums.get(i), nums.get(0) + nums.get(i) });
+      maximums.add(nums.get(0) + nums.get(i));
       List<Integer> arr = new ArrayList<>(nums);
       arr.remove(0);
       arr.remove(i - 1);
       if (arr.isEmpty()) {
-        total.add(new ArrayList<>(list));
+        total.add(new ArrayList<>(maximums));
       } else {
-        pair(arr, list, total);
+        pair(arr, maximums, total);
       }
     }
   }
 
   public int minPairSum(int[] nums) {
-    final List<List<int[]>> total = new ArrayList<>();
+    final List<List<Integer>> total = new ArrayList<>();
     pair(Arrays.stream(nums).boxed().collect(Collectors.toList()), new ArrayList<>(), total);
     final int[] f = new int[total.size()];
     for (int i = 0; i < total.size(); i++) {
-      final List<int[]> a = total.get(i);
-      int max = -1;
-      for (int[] b : a) {
-        final int sum = b[2];
-        if (max < sum) {
-          max = sum;
-          f[i] = max;
+      final List<Integer> maximums = total.get(i);
+      int maxFinal = -1;
+      for (int max : maximums) {
+        if (maxFinal < max) {
+          maxFinal = max;
+          f[i] = maxFinal;
         }
       }
     }
